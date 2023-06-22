eb21446160d2bd6dc056317802c2f0b105aa0088
nandhuaries <nandhu.s@ariesesolutions.com>
2023-06-22 15:16:07 +0530

    completed nomination form

---
+++ footer.php
@@ -0,0 +1,243 @@
+ <!-- Main Footer -->
+ <footer class="main-footer">
+     <!--Widgets Section-->
+     <div class="widgets-section">
+         <div class="auto-container">
+             <div class="row">
+                 <!--Big Column-->
+                 <div class="big-column col-xl-6 col-lg-12 col-md-12 col-sm-12">
+                     <div class="row">
+                         <!--Footer Column-->
+                         <div class="footer-column col-xl-7 col-lg-6 col-md-6 col-sm-12">
+                             <div class="footer-widget about-widget">
+                                 <div class="logo">
+                                     <a href="index.html"><img src="pics/logos/navLogo.png" alt="" /></a>
+                                 </div>
+                                 <!-- <div class="text">
+                      <p>
+                        We have very good strength in innovative technology
+                        and tools with over 35 years of experience. We makes
+                        long-term investments goal in global companies in
+                        different sectors, mainly in Europe and other
+                        countries.
+                      </p>
+                    </div> -->
+                                 <ul class="social-icon-one social-icon-colored">
+                                     <li>
+                                         <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                     </li>
+                                     <li>
+                                         <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                     </li>
+                                     <!-- <li>
+                        <a href="#"><i class="fab fa-pinterest"></i></a>
+                      </li>
+                      <li>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                      </li>
+                      <li>
+                        <a href="#"><i class="fab fa-dribbble"></i></a>
+                      </li> -->
+                                 </ul>
+                             </div>
+                         </div>
+
+                         <!--Footer Column-->
+                         <div class="footer-column col-xl-5 col-lg-6 col-md-6 col-sm-12">
+                             <div class="footer-widget useful-links">
+                                 <h2 class="widget-title">Useful Links</h2>
+                                 <ul class="user-links">
+                                     <li><a href="index-2.html">Home</a></li>
+                                     <li><a href="about-us.html">About Us</a></li>
+                                     <li><a href="services.html">Speakers</a></li>
+                                     <li><a href="projects.html">Sponsors</a></li>
+                                     <li><a href="blog.html">Gallery</a></li>
+                                     <li><a href="contact.html">Agenda</a></li>
+                                 </ul>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+
+                 <!--Big Column-->
+                 <div class="big-column col-xl-6 col-lg-12 col-md-12 col-sm-12">
+                     <div class="row">
+                         <!--Footer Column-->
+                         <div class="footer-column col-lg-6 col-md-6 col-sm-12">
+                             <!--Footer Column-->
+                             <div class="footer-widget contact-widget">
+                                 <h2 class="widget-title">Contact Us</h2>
+                                 <!--Footer Column-->
+                                 <div class="widget-content">
+                                     <ul class="contact-list">
+                                         <li>
+                                             <span class="icon flaticon-"></span>
+                                             <div class="text">
+                                                 Biz Events Management P O Box : 24496
+                                             </div>
+                                         </li>
+
+                                         <li>
+                                             <span class="icon flaticon-phone"></span>
+                                             <div class="text">
+                                                 <a href="tel:+971 6 5503300">+971 6 5503300</a>
+                                             </div>
+                                         </li>
+
+                                         <li>
+                                             <span class="icon flaticon-paper-plane"></span>
+                                             <div class="text">
+                                                 <a href="mailto:support@example.com">eventmanager@ariesgroupglobal.com</a>
+                                             </div>
+                                         </li>
+
+                                         <li>
+                                             <span class="icon flaticon-worldwide"></span>
+                                             <div class="text">
+                                                 Tower 400, 20th Floor Sharjah, UAE
+                                             </div>
+                                         </li>
+                                     </ul>
+                                 </div>
+                             </div>
+                         </div>
+
+
+                         <!-- <div class="footer-column col-lg-6 col-md-6 col-sm-12">
+
+                  <div class="footer-widget instagram-widget">
+                    <h2 class="widget-title">Gallery</h2>
+                    <div class="widget-content">
+                      <div class="outer clearfix">
+                        <figure class="image">
+                          <a href="images/gallery/1.jpg" class="lightbox-image" title="Image Title Here"><img
+                              src="images/resource/gw-1.jpg" alt="" /></a>
+                        </figure>
+
+                        <figure class="image">
+                          <a href="images/gallery/2.jpg" class="lightbox-image" title="Image Title Here"><img
+                              src="images/resource/gw-2.jpg" alt="" /></a>
+                        </figure>
+
+                        <figure class="image">
+                          <a href="images/gallery/3.jpg" class="lightbox-image" title="Image Title Here"><img
+                              src="images/resource/gw-3.jpg" alt="" /></a>
+                        </figure>
+
+                        <figure class="image">
+                          <a href="images/gallery/4.jpg" class="lightbox-image" title="Image Title Here"><img
+                              src="images/resource/gw-4.jpg" alt="" /></a>
+                        </figure>
+
+                        <figure class="image">
+                          <a href="images/gallery/5.jpg" class="lightbox-image" title="Image Title Here"><img
+                              src="images/resource/gw-5.jpg" alt="" /></a>
+                        </figure>
+
+                        <figure class="image">
+                          <a href="images/gallery/1.jpg" class="lightbox-image" title="Image Title Here"><img
+                              src="images/resource/gw-6.jpg" alt="" /></a>
+                        </figure>
+                      </div>
+                    </div>
+                  </div>
+                </div> -->
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+
+     <!--Footer Bottom-->
+     <div class="footer-bottom">
+         <div class="auto-container">
+             <div class="inner-container clearfix">
+                 <div class="copyright-text">
+                     <p>
+                         © Copyright 2024 All Rights Reserved Developed By
+                         <span style="color: red">&hearts;</span>
+
+                         <a href="https://www.ariesesolutions.com/">Aries e-Solutions</a>
+                     </p>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </footer>
+ <!-- End Footer -->
+ </div>
+ <!--End pagewrapper-->
+
+ <!-- Color Palate / Color Switcher -->
+ <div class="color-palate">
+     <div class="color-trigger">
+         <i class="fa fa-cog"></i>
+     </div>
+     <div class="color-palate-head">
+         <h6>Choose Your Demo</h6>
+     </div>
+     <ul class="box-version option-box">
+         <li>Full width</li>
+         <li class="box">Boxed</li>
+     </ul>
+     <ul class="rtl-version option-box">
+         <li>LTR Version</li>
+         <li class="rtl">RTL Version</li>
+     </ul>
+     <div class="palate-foo">
+         <span>You will find much more options for colors and styling in admin
+             panel. This color picker is used only for demonstation purposes.</span>
+     </div>
+     <a href="#" class="purchase-btn">Purchase now</a>
+ </div>
+ <!-- End Color Switcher -->
+
+ <!--Search Popup-->
+ <div id="search-popup" class="search-popup">
+     <div class="close-search theme-btn">
+         <span class="fas fa-window-close"></span>
+     </div>
+     <div class="popup-inner">
+         <div class="overlay-layer"></div>
+         <div class="search-form">
+             <form method="post" action="index.html">
+                 <div class="form-group">
+                     <fieldset>
+                         <input type="search" class="form-control" name="search-input" value="" placeholder="Search Here" required />
+                         <input type="submit" value="Search Now!" class="theme-btn" />
+                     </fieldset>
+                 </div>
+             </form>
+
+             <br />
+             <h3>Recent Search Keywords</h3>
+             <ul class="recent-searches">
+                 <li><a href="#">Seo</a></li>
+                 <li><a href="#">Bussiness</a></li>
+                 <li><a href="#">Events</a></li>
+                 <li><a href="#">Digital</a></li>
+                 <li><a href="#">Conferance</a></li>
+             </ul>
+         </div>
+     </div>
+ </div>
+
+ <!--Scroll to top-->
+ <div class="scroll-to-top scroll-to-target" data-target="html">
+     <span class="fa fa-angle-double-up"></span>
+ </div>
+ <script src="js/jquery.js"></script>
+ <script src="js/popper.min.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ <script src="js/jquery-ui.js"></script>
+ <script src="js/jquery.fancybox.js"></script>
+ <script src="js/appear.js"></script>
+ <script src="js/owl.js"></script>
+ <script src="js/jquery.countdown.js"></script>
+ <script src="js/wow.js"></script>
+ <script src="js/script.js"></script>
+ <!-- Color Setting -->
+ <script src="js/color-settings.js"></script>
+ </body>
+
+ </html>
